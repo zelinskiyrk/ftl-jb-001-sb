9b9c49104d3d759fc11af119ae44f689f271cb01
Prokurator <fiordi@yandex.ru>
2020-10-30 15:48:53 +0400

    L001 - HelloRestApiController

---
+++ src/main/java/com/zelinskiyrk/blog/base/routes/ExampleRoutes.java
@@ -0,0 +1,10 @@
+package com.zelinskiyrk.blog.base.routes;
+
+public class ExampleRoutes {
+    public static final String GET = BaseApiRoutes.v1 + "/get";
+    public static final String GET_WITH_PARAM = BaseApiRoutes.v1 + "/get-with-param";
+    public static final String GET_WITH_PATH = BaseApiRoutes.v1 + "/get-with-path-{pathVariable}";
+    public static final String PUT = BaseApiRoutes.v1 + "/put/{id}";
+    public static final String POST = BaseApiRoutes.v1 + "/post";
+    public static final String DELETE= BaseApiRoutes.v1 + "/delete/{id}";
+}
